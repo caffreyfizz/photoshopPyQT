0054b5a972b5f86fd1b31af8b95a4e95146329ba
Никита <107890115+caffreyfizz@users.noreply.github.com>
2023-10-24 02:15:57 +0300

    1. Улучшено отображение на дисплеях высокого разрешения 2. Добавлена "соль" паролей (пока не используется)

--- avtorization.py
+++ avtorization.py
@@ -1,8 +1,17 @@
 import sys
 import hashlib
 import sqlite3
+from random import choice, shuffle
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore, QtWidgets
+
+if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+
+if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+    
 
 
 class Registration(QMainWindow):
@@ -14,6 +23,8 @@ class Registration(QMainWindow):
         self.con = sqlite3.connect(db_name)
         self.cur = self.con.cursor()
 
+        self.salt_for_password = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
         self.registration_btn.clicked.connect(self.add_info)
     
     def add_info(self):
@@ -23,12 +34,17 @@ class Registration(QMainWindow):
                 self.errorLable.setText("Такой логин уже существует")
                 return
             
-            hashed_password = hashlib.sha1(self.passwordEdit.text().encode())
-            params = (self.loginEdit.text(), hashed_password.hexdigest(), "abcd")
+            salt = shuffle([choice(self.salt_for_password) for _ in range(5)])
+            hashed_password = hashlib.sha1(f"{self.passwordEdit.text() + salt}".encode())
+            params = (self.loginEdit.text(), hashed_password.hexdigest(), "abc")
             self.cur.execute(f"""INSERT INTO logins_and_passwords ('login', 'password', 'salt')
                                           VALUES (?, ?, ?)""", params).fetchall()
             self.errorLable.setText("Добро пожаловать!")
-            self.con.commit()                      
+            self.con.commit()  
+            self.close()
+        else:
+            self.errorLable.setText("Вы не ввели логин или пароль")
+            return
 
 
 if __name__ == '__main__':
